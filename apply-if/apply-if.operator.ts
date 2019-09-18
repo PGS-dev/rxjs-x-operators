7e6aa1290d16005049bbbe7b1702aae8affba157
Jakub Hajduk <kuba.hajduk@gmail.com>
2019-09-18 20:14:24 +0200

    Added params descriptions in operator descriptions

--- apply-if/apply-if.operator.ts
+++ apply-if/apply-if.operator.ts
@@ -5,6 +5,9 @@ import { Operators } from '../shared/types/operators';
 
 /**
  * applies truthyOperators to the pipe if condition is met. Otherwise applies falsyOperators
+ * @param condition condition that decides whether truthyOperators or falsyOperators will be applied to pipe
+ * @param truthyOperators operator, or array of operators that will be applied to pipe if condition is truthy
+ * @param falsyOperators operator, or array of operators that will be applied to pipe if condition is falsy
  */
 export function applyIf<T>(condition: boolean, truthyOperators: Operators, falsyOperators: Operators): MonoTypeOperatorFunction<T> {
     // @ts-ignore
